5c36056ff7398cfe06ba3a8b42a0380da1f684b8
Mohamed Saifeddine Kamoun <mohamedsaifeddine.kamoun@insat.ucar.tn>
2025-02-01 11:43:49 +0100

    new logo, background image, minor changes : with fixed event-app database line

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -20,7 +20,7 @@ dotenv.config()
       port: 3306,
       username: 'root',
       password: '',
-      database: 'eventplanner', // not event_app
+      database: 'event_app',
 
 
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
