29e23c8531b1dcd145d8b24969179af0e425c624
emna <emna.hasnaoui@insat.ucar.tn>
2025-02-04 21:54:18 +0100

    role based navbar items

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -7,15 +8,29 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  userId: number | null = null; // Variable to store the user ID
+
   constructor(private authService: AuthService) {}
-  
+
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (user: User) => {
+        this.userId = user.id;
+      },
+      error: (err) => {
+        console.error('Failed to fetch user:', err);
+      }
+    });
+  }
+
   isAuthenticated(): boolean {
     return this.authService.isAuthenticated();
   }
-
-  
   logout(): void {
     this.authService.logout();
   }
-
+  getUserRole(): string | null {
+    return localStorage.getItem('userRole');
+  }
+  
 }
