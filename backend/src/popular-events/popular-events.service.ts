d49d2ff5678e522ef37219dcae2ffd1bf0f50306
Mohamed Saifeddine Kamoun <mohamedsaifeddine.kamoun@insat.ucar.tn>
2025-02-05 13:39:18 +0100

    addaing backend part for popular events : popular events service

--- backend/src/popular-events/popular-events.service.ts
+++ backend/src/popular-events/popular-events.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EventRegistration } from 'src/event-registrations/entities/event-registration.entity/event-registration.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PopularEventsService {}
+export class PopularEventsService {
+
+    constructor(
+        @InjectRepository(EventRegistration)
+        private eventRegistrationRepository: Repository<EventRegistration>,
+        @InjectRepository(Event)
+        private eventRepository: Repository<Event>,  ) {}
+    async getPopularEvents(): Promise<Event[]> {
+        const popularEvents = await this.eventRegistrationRepository.query(
+          'select count(event_id) as count, event_id from event_registrations join event on event_registrations.event_id = event.id group by event_id order by count desc limit 3'
+        )
+        return popularEvents;
+      }
+}
